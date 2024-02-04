10bc6c95e83cb1923a59261c53d19ac307104218
Rajdeep Sarker <rajdeep.sarker@eviden.com>
2024-02-04 19:37:43 +0530

    Axios Json parse

--- src/Practice.js
+++ src/Practice.js
@@ -1,24 +1,61 @@
-import React, { createContext, useContext, useState } from 'react'
+import axios from "axios";
+import React, { createContext, useContext, useState } from "react";
 
 const PracticeContext = createContext();
 
+const PracticeContextProvider = (props) => {
+  const [brand, setBrand] = useState("Rebook");
 
-const Practice = () => {
+  const update = (object) => {
+    setBrand(object);
+  };
 
-    const onClickButton = ()=>{
-       console.log(color)
-       setColor("Yellow")
+  return (
+    <PracticeContext.Provider value={{ brand, update }}>
+      {props.children}
+    </PracticeContext.Provider>
+  );
+};
+
+const PracticeClildren = () => {
+  const aa = useContext(PracticeContext);
+
+  const onClickhandlr = async () => {
+    try {
+      const response = await axios.get("https://reqres.in/api/users?page=2");
+      console.log(response.data.data.length);
+      aa.update(JSON.stringify(response));
+    } catch (error) {
+      console.error(error);
     }
-
-const [color , setColor] = useState("Red");
+  };
 
   return (
     <div>
+      <h1>{aa.brand}</h1>
+      <button onClick={onClickhandlr}>Click</button>
+    </div>
+  );
+};
+
+const Practice = () => {
+  const onClickButton = () => {
+    console.log(color);
+    setColor("Yellow");
+  };
+
+  const [color, setColor] = useState("Red");
+
+  return (
+    <div>
+      <PracticeContextProvider>
+        <PracticeClildren></PracticeClildren>
+      </PracticeContextProvider>
       <h1>rajdeep={color}</h1>
 
       <button onClick={onClickButton}>Click Me</button>
     </div>
-  )
-}
+  );
+};
 
-export default Practice
+export default Practice;
