2ea685ee6c30ec36eb1b4d3e67415ae3c56c2d1b
Rajdeep Sarker <rajdeep.sarker@eviden.com>
2024-02-05 20:07:15 +0530

    UI Change

--- src/index.js
+++ src/index.js
@@ -8,11 +8,12 @@ import CreateContextparent from "./CreateContextparent";
 import ContextApiExample from "./ContextApiExample";
 import AxiosTest from "./AxiosTest";
 import Practice from "./Practice";
+import LayoutPractice from "./LayoutPractice";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Practice />
+    <LayoutPractice />
   </React.StrictMode>
 );
 
